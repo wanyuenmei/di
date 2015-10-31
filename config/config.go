1e4ab8f865d210a17118a2f755f75feff1fe99ad
Melvin Walls <mwalls67@gmail.com>
2015-10-31 14:08:15 -0700

    config: add parser (json for now) and poller
    This patch adds a config file, obviating the need for some of the flags
    defined before. After loading the initial config file, DI waits on
    notifications about changes to the config file and reacts by parsing the
    new config and updating things on the fly.

---
+++ config/config.go
@@ -0,0 +1,94 @@
+package config
+
+import (
+    "encoding/json"
+    "io/ioutil"
+
+    "gopkg.in/fsnotify.v1"
+    "github.com/op/go-logging"
+    "github.com/NetSys/di/cluster"
+)
+
+type di_config struct {
+    RedCount int
+    BlueCount int
+    HostCount int           /* Number of VMs */
+    CloudConfig string      /* Path to cloud config */
+    Region string           /* AWS availability zone */
+}
+
+var log = logging.MustGetLogger("config")
+
+func parseConfig(config_path *string) *cluster.Config {
+    var temp_config di_config
+    var config cluster.Config
+
+    config_file, err := ioutil.ReadFile(*config_path)
+    if err != nil {
+        log.Warning("Error reading config")
+        log.Warning(err.Error())
+        return nil
+    }
+
+    err = json.Unmarshal(config_file, &temp_config)
+    if err != nil {
+        log.Warning("Malformed config")
+        log.Warning(err.Error())
+        return nil
+    }
+
+    config.InstanceCount = temp_config.HostCount
+    config.Region = temp_config.Region
+    config.CloudConfig, err = ioutil.ReadFile(temp_config.CloudConfig)
+    if err != nil {
+        log.Warning("Error reading cloud config")
+        log.Warning(err.Error())
+        return nil
+    }
+
+    return &config
+}
+
+func watchConfigForUpdates(config_path *string,
+                           config_chan chan *cluster.Config) {
+    watcher, err := fsnotify.NewWatcher()
+    if err != nil {
+        panic(err)
+    }
+    defer watcher.Close()
+
+    err = watcher.Add(*config_path)
+    if err != nil {
+        panic(err)
+    }
+
+    /* Read initial config */
+    new_config := parseConfig(config_path)
+    config_chan <- new_config
+
+    for {
+        select {
+            case e := <-watcher.Events:
+                new_config := parseConfig(&e.Name)
+                config_chan <- new_config
+                /* XXX: Some editors (e.g. vim) trigger a rename event, even
+                 * if the filename doesn't actually change. This results in the
+                 * old listener becoming stale. If there's a cleaner way to do
+                 * this, let's replace this. */
+                if e.Op == fsnotify.Rename {
+                    watcher.Remove(e.Name)
+                    watcher.Add(*config_path)
+                }
+            case err := <-watcher.Errors:
+                panic(err)
+            default:
+                continue
+        }
+    }
+}
+
+func WatchConfig(config_path *string) chan *cluster.Config {
+    config_chan := make(chan *cluster.Config)
+    go watchConfigForUpdates(config_path, config_chan)
+    return config_chan
+}
--- di.go
+++ di.go
@@ -3,11 +3,9 @@ package main
 import (
     "flag"
     "fmt"
-    "io/ioutil"
-    "os"
-    "strconv"
     "time"
 
+    "github.com/NetSys/di/config"
     "github.com/NetSys/di/cluster"
     "github.com/op/go-logging"
 )
@@ -16,46 +14,36 @@ var log = logging.MustGetLogger("main")
 
 func main() {
     flag.Usage = func() {
-        fmt.Fprintf(os.Stderr, "%s: count\n", os.Args[0])
         flag.PrintDefaults()
     }
 
+    var config_path = flag.String("c", "config.json", "path to config file")
     flag.Parse()
-    args := flag.Args()
 
-    if len(args) < 1 {
-        panic("Number of requested instances requred.")
-    }
+    var the_config *cluster.Config
+    config_chan := config.WatchConfig(config_path) 
 
     /* XXX: There's research in this somewhere.  How do we validate inputs into
     * the policy?  What do we do with a policy that's wrong?  Also below, we
     * want someone to be able to say "limit the number of instances for cost
     * reasons" ... look at what's going on in amp for example.  100k in a month
     * is crazy. */
-    count, err := strconv.ParseInt(args[0], 10, 32)
-    if err != nil {
-        panic("Could not parse count.")
-    }
 
-    if count < 0 || count > 10 {
-        panic("Invalid Count")
+    /* Block until the initial config has been parsed. */
+    the_config = <-config_chan
+    if the_config == nil {
+        log.Fatal("Failed to read ", *config_path)
     }
+    aws := cluster.New(cluster.AWS, the_config.Region)
+    aws.UpdateConfig(*the_config)
 
     log.Info("Starting")
 
-    cloud_config, err := ioutil.ReadFile("cloud-config.yaml")
-    if err != nil {
-        panic(err)
-    }
-
-    aws := cluster.New(cluster.AWS, "us-west-2")
-
-    aws.UpdateConfig(cluster.Config {
-        InstanceCount: int(count),
-        CloudConfig: cloud_config,
-    })
-
     for {
+        select {
+            case the_config = <-config_chan:
+                aws.UpdateConfig(*the_config)
+        }
         fmt.Println(aws.GetStatus())
         time.Sleep(10 * time.Second)
     }
