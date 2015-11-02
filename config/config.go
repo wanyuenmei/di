b78ff0ec5472a1790722f8047ecb8adf3cb0fa8f
Ethan J. Jackson <ejj@eecs.berkeley.edu>
2015-11-02 10:32:54 -0800

    config: Cleanup implementation.
    This patch makes a couple of cleanliness tweaks to the config
    implementation.
    
    First, it makes it so the config channel never
    returns nil.  This means that callers don't have to hand roll error
    handling simplifying the code.
    
    Second, config.go was using pointers to strings in several places
    where a string would suffice.
    
    Third, it moves a comment into config.go from di.go.

--- config/config.go
+++ config/config.go
@@ -19,11 +19,11 @@ type di_config struct {
 
 var log = logging.MustGetLogger("config")
 
-func parseConfig(config_path *string) *cluster.Config {
+func parseConfig(config_path string) *cluster.Config {
     var temp_config di_config
     var config cluster.Config
 
-    config_file, err := ioutil.ReadFile(*config_path)
+    config_file, err := ioutil.ReadFile(config_path)
     if err != nil {
         log.Warning("Error reading config")
         log.Warning(err.Error())
@@ -37,6 +37,11 @@ func parseConfig(config_path *string) *cluster.Config {
         return nil
     }
 
+    /* XXX: There's research in this somewhere.  How do we validate inputs into
+    * the policy?  What do we do with a policy that's wrong?  Also below, we
+    * want someone to be able to say "limit the number of instances for cost
+    * reasons" ... look at what's going on in amp for example.  100k in a month
+    * is crazy. */
     config.InstanceCount = temp_config.HostCount
     config.Region = temp_config.Region
     config.CloudConfig, err = ioutil.ReadFile(temp_config.CloudConfig)
@@ -49,35 +54,40 @@ func parseConfig(config_path *string) *cluster.Config {
     return &config
 }
 
-func watchConfigForUpdates(config_path *string,
-                           config_chan chan *cluster.Config) {
+func watchConfigForUpdates(config_path string,
+                           config_chan chan cluster.Config) {
     watcher, err := fsnotify.NewWatcher()
     if err != nil {
         panic(err)
     }
     defer watcher.Close()
 
-    err = watcher.Add(*config_path)
+    err = watcher.Add(config_path)
     if err != nil {
         panic(err)
     }
 
     /* Read initial config */
     new_config := parseConfig(config_path)
-    config_chan <- new_config
+    if new_config != nil {
+        config_chan <- *new_config
+    }
 
     for {
         select {
             case e := <-watcher.Events:
-                new_config := parseConfig(&e.Name)
-                config_chan <- new_config
+                new_config := parseConfig(e.Name)
+                if new_config != nil {
+                    config_chan <- *new_config
+                }
+
                 /* XXX: Some editors (e.g. vim) trigger a rename event, even
                  * if the filename doesn't actually change. This results in the
                  * old listener becoming stale. If there's a cleaner way to do
                  * this, let's replace this. */
                 if e.Op == fsnotify.Rename {
                     watcher.Remove(e.Name)
-                    watcher.Add(*config_path)
+                    watcher.Add(config_path)
                 }
             case err := <-watcher.Errors:
                 panic(err)
@@ -87,8 +97,8 @@ func watchConfigForUpdates(config_path *string,
     }
 }
 
-func WatchConfig(config_path *string) chan *cluster.Config {
-    config_chan := make(chan *cluster.Config)
+func WatchConfig(config_path string) chan cluster.Config {
+    config_chan := make(chan cluster.Config)
     go watchConfigForUpdates(config_path, config_chan)
     return config_chan
 }
--- di.go
+++ di.go
@@ -20,29 +20,16 @@ func main() {
     var config_path = flag.String("c", "config.json", "path to config file")
     flag.Parse()
 
-    var the_config *cluster.Config
-    config_chan := config.WatchConfig(config_path) 
-
-    /* XXX: There's research in this somewhere.  How do we validate inputs into
-    * the policy?  What do we do with a policy that's wrong?  Also below, we
-    * want someone to be able to say "limit the number of instances for cost
-    * reasons" ... look at what's going on in amp for example.  100k in a month
-    * is crazy. */
-
-    /* Block until the initial config has been parsed. */
-    the_config = <-config_chan
-    if the_config == nil {
-        log.Fatal("Failed to read ", *config_path)
-    }
-    aws := cluster.New(cluster.AWS, the_config.Region)
-    aws.UpdateConfig(*the_config)
-
     log.Info("Starting")
+    config_chan := config.WatchConfig(*config_path)
+    the_config := <-config_chan
+    aws := cluster.New(cluster.AWS, the_config.Region)
+    aws.UpdateConfig(the_config)
 
     for {
         select {
             case the_config = <-config_chan:
-                aws.UpdateConfig(*the_config)
+                aws.UpdateConfig(the_config)
         }
         fmt.Println(aws.GetStatus())
         time.Sleep(10 * time.Second)
