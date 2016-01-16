cb53c0439a1f2dea27d1c65b7bbe9db62435ceb4
Ethan J. Jackson <ejj@eecs.berkeley.edu>
2016-01-15 17:04:17 -0800

    minion: Leader election campaign improvements
    This patch makes several improvements to the leader election campaign
    including, logging when there are problems, delaying the first
    election cycle until etcd has had time to come up, and un-setting the
    leader flag on a role change.

--- minion/elector.go
+++ minion/elector.go
@@ -8,12 +8,13 @@ import (
 	"github.com/coreos/etcd/client"
 )
 
-const ttl = 30
+const electionTTL = 30
+const bootDelay = 30
 const leaderKey = "/minion/leader"
 
 func watchLeader(conn db.Conn) {
 	kapi, watch := etcdConnect()
-	trigg := conn.TriggerTick(ttl, db.MinionTable)
+	trigg := conn.TriggerTick(electionTTL, db.MinionTable)
 	for {
 		resp, _ := kapi.Get(ctx(), leaderKey, &client.GetOptions{Quorum: true})
 
@@ -39,11 +40,33 @@ func watchLeader(conn db.Conn) {
 }
 
 func campaign(conn db.Conn) {
+	trigg := conn.TriggerTick(electionTTL/2, db.MinionTable)
 	kapi, watchChan := etcdConnect()
-	trigg := conn.TriggerTick(ttl/2, db.MinionTable)
+	oldMaster := false
+
 	for {
+		select {
+		case <-watchChan:
+		case <-trigg.C:
+		}
+
 		minions := conn.SelectFromMinion(nil)
-		if len(minions) != 1 || minions[0].Role != db.Master {
+		master := len(minions) == 1 && minions[0].Role == db.Master
+		switch {
+		case oldMaster && !master:
+			commitLeader(conn, false, "")
+		case !oldMaster && master:
+			// When we first boot, wait a bit for etcd to come up.
+			log.Info("Starting leader election in %d seconds", bootDelay)
+			time.Sleep(bootDelay * time.Second)
+
+			// Update in case something changed while we were sleeping
+			minions = conn.SelectFromMinion(nil)
+			master = len(minions) == 1 && minions[0].Role == db.Master
+		}
+		oldMaster = master
+
+		if !master {
 			continue
 		}
 
@@ -53,26 +76,47 @@ func campaign(conn db.Conn) {
 		}
 
 		opts := client.SetOptions{PrevExist: client.PrevNoExist,
-			TTL: ttl * time.Second}
+			TTL: electionTTL * time.Second}
 		if minions[0].Leader {
 			opts.PrevExist = client.PrevExist
 		}
 
 		_, err := kapi.Set(ctx(), leaderKey, IP, &opts)
-		conn.Transact(func(view db.Database) error {
-			minions := view.SelectFromMinion(nil)
-			if len(minions) == 1 {
-				minions[0].Leader = err == nil
-				view.Commit(minions[0])
-			}
-			return nil
-		})
+		if err == nil {
+			commitLeader(conn, true, IP)
+		} else {
+			clientErr, ok := err.(client.Error)
+			if !ok || clientErr.Code != client.ErrorCodeNodeExist {
+				log.Warning("Error setting leader key: %s", err.Error())
+				commitLeader(conn, false, "")
 
-		select {
-		case <-watchChan:
-		case <-trigg.C:
+				// Give things a chance to settle down.
+				time.Sleep(electionTTL * time.Second)
+			} else {
+				commitLeader(conn, false)
+			}
 		}
 	}
+}
+
+func commitLeader(conn db.Conn, leader bool, ip ...string) {
+	if len(ip) > 1 {
+		panic("Not Reached")
+	}
+
+	conn.Transact(func(view db.Database) error {
+		minions := view.SelectFromMinion(nil)
+		if len(minions) == 1 {
+			minions[0].Leader = leader
+
+			if len(ip) == 1 {
+				minions[0].LeaderIP = ip[0]
+			}
+
+			view.Commit(minions[0])
+		}
+		return nil
+	})
 }
 
 func etcdConnect() (client.KeysAPI, <-chan struct{}) {
@@ -106,6 +150,6 @@ func etcdConnect() (client.KeysAPI, <-chan struct{}) {
 }
 
 func ctx() context.Context {
-	ctx, _ := context.WithTimeout(context.Background(), (ttl/4)*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), (electionTTL/4)*time.Second)
 	return ctx
 }
