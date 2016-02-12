5507606bbd5fd747ac10472e5d1b7af5c45f8e39
Ethan J. Jackson <ejj@eecs.berkeley.edu>
2016-02-11 18:48:24 -0800

    ubuntu: Fix the docker version to 1.9.1
    When docker released version 1.10 it broke the minion.  For now we'll
    fix our docker version at 1.9 so we don't have to worry about constant
    docker updates.

--- util/util.go
+++ util/util.go
@@ -50,7 +50,7 @@ ExecStart=/usr/bin/docker daemon \
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" > /etc/apt/sources.list.d/docker.list
 apt-get update
-apt-get install docker-engine -y
+apt-get install docker-engine=1.9.1-0~wily -y
 
 /usr/bin/docker run -d --net=host --name=minion --privileged -v /var/run/docker.sock:/var/run/docker.sock %s
 
