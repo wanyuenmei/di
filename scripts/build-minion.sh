aa851c0719005d32d681c4b18160b6474d78cece
Nicholas Sun <olassun2@gmail.com>
2016-02-22 20:30:07 -0800

    travis: Fix docker login
    Improve message printing format, also have docker print version
    information.

--- scripts/build-minion.sh
+++ scripts/build-minion.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
+function status_line() {
+    echo -e "\n### ${1} ###\n"
+}
+
 # Exit upon any error
 set -e
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    echo "This is a pull request, not building minion."
+    status_line "This is a pull request, not building minion."
     exit 0
 fi
 
 if [ "$TRAVIS_BRANCH" != "master" ]; then
-    echo "This is not the master branch, not building minion."
+    status_line "This is not the master branch, not building minion."
     exit 0
 fi
 
-echo "Building minion..."
+docker version
+
+status_line "Building minion..."
 
 make docker
-echo "Successfully built minion."
+status_line "Successfully built minion."
 
-docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-echo "Successfully logged into docker."
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" quay.io
+status_line "Successfully logged into docker."
 
 docker push quay.io/netsys/di-minion
-echo "Successfully pushed minion."
+status_line "Successfully pushed minion."
