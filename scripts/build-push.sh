9ca1ff4bb2b43af14f8427bc94d14e81eb068222
Nicholas Sun <olassun2@gmail.com>
2016-02-23 17:34:00 -0800

    travis: Have the minion container be built on every run

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -7,22 +7,28 @@ function status_line() {
 # Exit upon any error
 set -e
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    status_line "This is a pull request, not building minion."
-    exit 0
-fi
+status_line "Begin build..."
 
-if [ "$TRAVIS_BRANCH" != "master" ]; then
-    status_line "This is not the master branch, not building minion."
-    exit 0
-fi
-
-docker version
+make all check
 
 status_line "Building minion..."
 
 make docker
 status_line "Successfully built minion."
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    status_line "This is a pull request, not pushing minion."
+    exit 0
+fi
+
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+    status_line "This is not the master branch, not pushing minion."
+    exit 0
+fi
+
+docker version
+
+status_line "Pushing minion..."
 
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" quay.io
 status_line "Successfully logged into docker."
