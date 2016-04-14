fa9e183afdf74b94befd8edd208d31dd92845486
Kevin Lin <kaikai526@gmail.com>
2016-04-14 13:29:39 -0700

    config_test: Add wordpress spec

--- config_test.go
+++ config_test.go
@@ -42,4 +42,5 @@ func TestConfigs(t *testing.T) {
 	testConfig("di-tester/config/config.spec")
 	testConfig("specs/spark/spark.spec")
 	testConfig("specs/zookeeper/zookeeper.spec")
+	testConfig("specs/wordpress/wordpress.spec")
 }
