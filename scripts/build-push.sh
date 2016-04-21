295b07a2faa57533fceda4d21a2f9e6b615ef2e8
Nicholas Sun <olassun2@gmail.com>
2016-04-20 18:38:34 -0700

    ci: Update travis to use new make targets
    Also update the print messages.

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -15,33 +15,27 @@ if [[ $(make -s lint 2>&1) ]] ; then # golint doesn't fail, just prints things.
     exit 1
 fi
 
-status_line "Building minion..."
+status_line "Building containers..."
 
 make docker-build-di docker-build-tester docker-build-minion
-status_line "Successfully built minion."
+status_line "Successfully built containers."
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-    status_line "This is a pull request, not pushing minion."
+    status_line "This is a pull request, not pushing containers."
     exit 0
 fi
 
 if [ "$TRAVIS_BRANCH" != "master" ]; then
-    status_line "This is not the master branch, not pushing minion."
+    status_line "This is not the master branch, not pushing containers."
     exit 0
 fi
 
 docker version
 
-status_line "Pushing minion..."
+status_line "Pushing containers..."
 
 docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" quay.io
 status_line "Successfully logged into docker."
 
-docker push quay.io/netsys/di
-status_line "Successfully pushed di."
-
-docker push quay.io/netsys/di-minion
-status_line "Successfully pushed minion."
-
-docker push quay.io/netsys/di-tester
-status_line "Successfully pushed tester."
+make docker-push-di docker-push-tester docker-push-minion
+status_line "Successfully pushed containers."
