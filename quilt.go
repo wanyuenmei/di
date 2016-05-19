80a8391e7bc618843b22b02bc929495402ba0d2a
Matthew Mussomele <mmussomele@berkeley.edu>
2016-05-18 22:53:38 -0700

    quilt: Add command line support for selecting log level

--- quilt.go
+++ quilt.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	l_mod "log"
 	"os"
@@ -29,12 +31,27 @@ func main() {
 
 	log.SetFormatter(util.Formatter{})
 
+	validLevels := "Valid logger levels are:\n" +
+		"    debug, info, warn, error, fatal or panic."
+
 	flag.Usage = func() {
+		fmt.Println("Usage: quilt [-c=configpath] [-log-level=level | -l=level]")
 		flag.PrintDefaults()
+		fmt.Println(validLevels)
 	}
 
 	var configPath = flag.String("c", "config.spec", "path to config file")
+	var logLevel = flag.String("log-level", "info", "level to set logger to")
+	flag.StringVar(logLevel, "l", "info", "level to set logger to")
 	flag.Parse()
+
+	level, err := parseLogLevel(*logLevel)
+	if err != nil {
+		fmt.Println(err)
+		flag.Usage()
+		os.Exit(1)
+	}
+	log.SetLevel(level)
 
 	conn := db.New()
 	go func() {
@@ -76,4 +93,26 @@ func updateConfig(conn db.Conn, configPath string) error {
 	}
 
 	return engine.UpdatePolicy(conn, spec)
+}
+
+// parseLogLevel returns the log.Level type corresponding to the given string
+// (case insensitive).
+// If no such matching string is found, it returns log.InfoLevel (default) and an error.
+func parseLogLevel(logLevel string) (log.Level, error) {
+	logLevel = strings.ToLower(logLevel)
+	switch logLevel {
+	case "debug":
+		return log.DebugLevel, nil
+	case "info":
+		return log.InfoLevel, nil
+	case "warn":
+		return log.WarnLevel, nil
+	case "error":
+		return log.ErrorLevel, nil
+	case "fatal":
+		return log.FatalLevel, nil
+	case "panic":
+		return log.PanicLevel, nil
+	}
+	return log.InfoLevel, errors.New(fmt.Sprintf("bad log level: '%v'", logLevel))
 }
